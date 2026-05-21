8e1005ebb7ef90d4f7f364c418db2af6a8d04033
timeoblrynov <timeo.baffreauleroux@ynov.com>
2026-05-21 20:14:22 +0200

    add : room frontend

--- app/(protected)/room/page.tsx
+++ app/(protected)/room/page.tsx
@@ -6,6 +6,8 @@ import { Permissions } from "@/lib/config";
 import { useNavData } from "@/stores/store";
 import { useEffect, useState } from "react";
 import { BiPlusCircle } from "react-icons/bi";
+import { VscCircleLargeFilled } from "react-icons/vsc";
+
 
 export default function Home() {
     const { call } = useApi()
@@ -43,15 +45,26 @@ export default function Home() {
 
     return (
         <div>
-            <h2 className="text-center text-white/60 text-[20px]">Système de room en développement !</h2>
-            <div className="flex items-center gap-2 w-1/3">
-                <input type="text" className="p-2 bg-[#212529] text-xs outline-none border text-white/60 border-white/5 focus:border-white/50 transition duration-500 w-1/5" placeholder="Insert the room code" value={roomCode} onChange={(e) => setRoomCode(e.target.value)} />
-                <button onClick={joinRoom} className="w-1/5 cursor-pointer flex items-center justify-center gap-3 border-2 border-white/40 text-white/40 font-mono text-[20px] hover:bg-white/40 hover:border-white/40 hover:text-white transition duration-500">Join</button>
-                {Array.isArray(permissions) && permissions.includes(Permissions.advanced.administrator) && <button onClick={() => setCreateRoom(true)} className="w-fit px-3 cursor-pointer flex items-center justify-center gap-3 border-2 border-white/40 text-white/40 font-mono text-[20px] hover:bg-white/40 hover:border-white/40 hover:text-white transition duration-500"><BiPlusCircle className="text-white/40" />Create room</button>}
+            <h2 className="text-center text-white/30 text-[35px] font-mono mt-20 mb-15">To access a room, please enter your access code :</h2>
+            <div className="flex gap-10 w-1/3 mb-20 mx-auto">
+                <input type="text" className="w-full border-2 font-mono text-[20px] border-white/40 text-white/80 p-1.5 placeholder:text-white/25" placeholder="Insert the room code" value={roomCode} onChange={(e) => setRoomCode(e.target.value)} />
+                <button onClick={joinRoom} className="cursor-pointer text-[20px] flex p-2 items-center justify-center gap-3 border-2 border-white/40 text-white/40 font-mono hover:bg-white/40 hover:border-white/40 hover:text-white transition duration-500">JOIN</button>
             </div>
-            <div className="space-y-4 mt-5 flex flex-col gap-5">
-                <div className="flex items-center gap-3 text-xs uppercase tracking-[0.2em] text-white/30"><span className="h-px w-6 bg-white/20" />Rooms active<span className="h-px flex-1 bg-white/10" /></div>
-                <div className="flex items-center gap-3 text-xs uppercase tracking-[0.2em] text-white/30"><span className="h-px w-6 bg-white/20" />Ancienne rooms<span className="h-px flex-1 bg-white/10" /></div>
+            {Array.isArray(permissions) && permissions.includes(Permissions.advanced.administrator) && <button onClick={() => setCreateRoom(true)} className="w-fit px-3 mb-25 cursor-pointer flex items-center mx-auto gap-3 border-2 border-white/40 p-2 text-white/40 font-mono text-[20px] hover:bg-white/40 hover:border-white/40 hover:text-white transition duration-500"><BiPlusCircle size={20} className="text-white/40" />Create room</button>}
+            <div className="space-y-4 mt-5 flex flex-col gap-5 font-mono">            
+                <div className="flex mx-auto gap-30">
+                    <div className="flex-col items-center gap-3 text-[20px] uppercase border-2 border-white/40 tracking-[0.2em] text-white/30">
+                        <h2 className="flex items-center gap-6 mx-50 p-2"><VscCircleLargeFilled size={15} className="text-green-400" />Active Rooms</h2>
+                        <div className="border-t-2"></div>
+                        <p>test</p>
+                    </div>
+                    <div className="flex-col items-cente gap-3 text-[20px] uppercase border-2 border-white/40 tracking-[0.2em] text-white/30">
+                        <h2 className="flex items-center gap-6 mx-50 p-2"><VscCircleLargeFilled size={15} className="text-red-400" />Old Rooms</h2>
+                        <div className="border-t-2"></div>
+                        <p>test</p>
+                    </div>
+                </div>
+
             </div>
             {createRoom && <RoomBuilder onCreate={v => handleCreate(v)} onClose={() => setCreateRoom(false)}/>}
         </div>
