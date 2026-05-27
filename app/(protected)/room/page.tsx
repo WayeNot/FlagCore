57afcbcf24a691b0f01e5f5e889508aee99247c9
timeoblrynov <timeo.baffreauleroux@ynov.com>
2026-05-27 10:50:35 +0200

    add

--- app/(protected)/room/page.tsx
+++ app/(protected)/room/page.tsx
@@ -46,7 +46,7 @@ export default function Home() {
     return (
         <div>
             <div className="border-t-2 border-b-2 my-15 border-white/30">
-                <h2 className="text-center text-white/30 text-[35px] font-mono mt-10 mb-15">To access a room, please enter your access code :</h2>
+                <h2 className="text-center text-white/50 text-[35px] font-mono mt-10 mb-15">To access a room, please enter your access code :</h2>
                 <div className="flex gap-10 w-1/3 mb-20 mx-auto">
                     <input type="text" className="w-full border-2 font-mono text-[20px] border-white/40 text-white/80 p-1.5 placeholder:text-white/25" placeholder="Insert the room code" value={roomCode} onChange={(e) => setRoomCode(e.target.value)} />
                     <button onClick={joinRoom} className="cursor-pointer text-[20px] flex p-2 items-center justify-center gap-3 border-2 border-white/40 text-white/40 font-mono hover:bg-white/40 hover:border-white/40 hover:text-white transition duration-500">JOIN</button>
@@ -56,12 +56,12 @@ export default function Home() {
             <div className="space-y-4 mt-5 flex flex-col gap-5 font-mono">            
                 <div className="flex mx-auto gap-30">
                     <div className="flex-col items-center gap-3 text-[20px] uppercase border-2 border-white/40 tracking-[0.2em] text-white/30">
-                        <h2 className="flex items-center gap-6 mx-50 p-2"><VscCircleLargeFilled size={15} className="text-green-400" />Active Rooms</h2>
+                        <h2 className="flex items-center gap-6 mx-50 p-2 font-bold"><VscCircleLargeFilled size={15} className="text-green-400" />Active Rooms</h2>
                         <div className="border-t-2"></div>
                         <p>test</p>
                     </div>
                     <div className="flex-col items-cente gap-3 text-[20px] uppercase border-2 border-white/40 tracking-[0.2em] text-white/30">
-                        <h2 className="flex items-center gap-6 mx-50 p-2"><VscCircleLargeFilled size={15} className="text-red-400" />Old Rooms</h2>
+                        <h2 className="flex items-center gap-6 mx-50 p-2 font-bold"><VscCircleLargeFilled size={15} className="text-red-400" />Old Rooms</h2>
                         <div className="border-t-2"></div>
                         <p>test</p>
                     </div>
--- app/(protected)/team/page.tsx
+++ app/(protected)/team/page.tsx
@@ -1,7 +1,86 @@
-"use client"
+"use client";
 
-export default function Home() {
-    return (
-        <div>Coming soon..</div>
-    )
+import "leaflet/dist/leaflet.css";
+
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents,
+} from "react-leaflet";
+
+import L from "leaflet";
+import { useState } from "react";
+
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+  iconUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  shadowUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+});
+
+type MarkerType = {
+  lat: number;
+  lng: number;
+};
+
+let count = 0
+
+function ClickHandler({
+  onAddMarker,
+}: {
+  onAddMarker: (marker: MarkerType) => void;
+}) {
+  useMapEvents({
+    click(e) {
+      onAddMarker({
+        lat: e.latlng.lat,
+        lng: e.latlng.lng,
+      });
+    },
+  });
+
+  return null;
+}
+
+export default function GameMap() {
+  const [markers, setMarkers] = useState<MarkerType[]>([
+    { lat: 47.2057585, lng: -1.5390026 },
+  ]);
+
+const addMarker = (marker: MarkerType) => {
+    setMarkers((prev) => [...prev, marker]);
+    count += 1
+};
+
+  return (
+    <MapContainer
+      center={[48.8566, 2.3522]}
+      zoom={5}
+      style={{ height: "600px", width: "100%" }}
+    >
+      <TileLayer
+        attribution='&copy; OpenStreetMap contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+
+      <ClickHandler onAddMarker={addMarker} />
+
+      {markers.map((marker, index) => (
+        <Marker
+          key={index}
+          position={[marker.lat, marker.lng]}
+        >
+          <Popup>
+            Marker {index + 1}
+          </Popup>
+        </Marker>
+      ))}
+    </MapContainer>
+  );
 }
